127dedf0f81ee18aa2a9f5ea26fb7d1359d74896
lexno <lexno23@gmail.com>
2018-09-27 14:55:00 +0800

    -- 添加日志 -- 添加mybatis生成数据库操作

--- shop.sql
+++ shop.sql
@@ -1,106 +1,170 @@
-CREATE TABLE `t_admin` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL COMMENT '用户名',
-  `password` varchar(255) DEFAULT NULL  COMMENT '密码',
-  `gmt_create` datetime DEFAULT DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `gmt_modified` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT '管理员';
+/*
+ Navicat Premium Data Transfer
 
-CREATE TABLE `t_announcement` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT  COMMENT 'id',
-  `content` varchar(255) DEFAULT NULL  COMMENT '内容',
-  `is_deleted` tinyint DEFAULT 1 COMMENT '是否删除',
-  `gmt_create` datetime DEFAULT DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `gmt_modified` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '公告';
+ Source Server         : 本地MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 50723
+ Source Host           : localhost:3306
+ Source Schema         : shop
 
-CREATE TABLE `t_news` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'id' ,
-  `title` varchar(255) DEFAULT NULL COMMENT '标题',
-  `content` varchar(255) DEFAULT NULL COMMENT '内容',
-  `is_deleted` tinyint DEFAULT 1 COMMENT '是否删除',
-  `gmt_create` datetime DEFAULT DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `gmt_modified` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8  COMMENT '新闻';
+ Target Server Type    : MySQL
+ Target Server Version : 50723
+ File Encoding         : 65001
 
-CREATE TABLE `t_product` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `title` varchar(255) DEFAULT NULL COMMENT '商品标题',
-  `code` varchar(255) DEFAULT NULL COMMENT '商品码',
-  `model` varchar(255) DEFAULT NULL COMMENT '型号',
-  `note` varchar(255) DEFAULT NULL COMMENT '商品描述',
-  `pic_url` varchar(255) DEFAULT NULL COMMENT '商品图片',
-  `price` int(11) DEFAULT NULL COMMENT '价格',
-  `stock` bigint(20) DEFAULT NULL COMMENT '库存',
-  `is_deleted` tinyint DEFAULT 1 COMMENT '是否删除',
-  `gmt_create` datetime DEFAULT DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `gmt_modified` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT '商品';
+ Date: 27/09/2018 10:00:08
+*/
 
-CREATE TABLE `t_producttype` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `name` varchar(255) DEFAULT NULL COMMENT '类别名称',
-  `is_deleted` tinyint DEFAULT 1 COMMENT '是否删除',
-  `gmt_create` datetime DEFAULT DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `gmt_modified` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT '商品类别';
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE TABLE `t_user` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '用户id',
-  `account` varchar(255) DEFAULT NULL COMMENT '余额',
-  `address` varchar(50) DEFAULT NULL COMMENT '收货地址',
-  `password` varchar(14) NOT NULL COMMENT '密码',
-  `phone` varchar(11) DEFAULT NULL COMMENT '电话号码',
-  `point` int(11) DEFAULT NULL COMMENT '积分值',
-  `remark` varchar(50) DEFAULT NULL COMMENT '备注',
-  `tel_phone` varchar(11) DEFAULT NULL COMMENT '座机',
-  `username` varchar(15) DEFAULT NULL COMMENT '用户名',
-  `zip_code` varchar(6) DEFAULT NULL COMMENT '邮编',
-  `is_deleted` tinyint DEFAULT 1 COMMENT '是否删除',
-  `gmt_create` datetime DEFAULT DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `gmt_modified` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT '用户';
+-- ----------------------------
+-- Table structure for t_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `t_admin`;
+CREATE TABLE `t_admin`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `gmt_create` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '管理员' ROW_FORMAT = Dynamic;
 
-CREATE TABLE `t_useraddress` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `address` varchar(255) DEFAULT NULL  COMMENT '地址',
-  `consignee` varchar(255) DEFAULT NULL COMMENT '收货人姓名',
-  `phone` varchar(255) DEFAULT NULL  COMMENT '收货电话',
-  `zipcode` varchar(255) DEFAULT NULL COMMENT '邮编',
-  `user_id` bigint unsigned DEFAULT NULL COMMENT '用户id',
-  `is_deleted` tinyint DEFAULT 1 COMMENT '是否删除',
-  `gmt_create` datetime DEFAULT DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `gmt_modified` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '地址';
+-- ----------------------------
+-- Table structure for t_announcement
+-- ----------------------------
+DROP TABLE IF EXISTS `t_announcement`;
+CREATE TABLE `t_announcement`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '内容',
+  `is_deleted` tinyint(4) NULL DEFAULT 1 COMMENT '是否删除',
+  `gmt_create` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '公告' ROW_FORMAT = Dynamic;
 
-CREATE TABLE `t_order` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `order_number` varchar(255) DEFAULT NULL COMMENT 'id',
-  `status` int(11) DEFAULT NULL COMMENT '状态',
-  `user_id` bigint unsigned DEFAULT NULL  COMMENT '购买人id',
-  `user_address` bigint unsigned DEFAULT NULL  COMMENT '收货地址',
-  `is_deleted` tinyint DEFAULT 1 COMMENT '是否删除',
-  `gmt_create` datetime DEFAULT DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `gmt_modified` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`user_address`) REFERENCES `t_useraddress` (`id`),
-  FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '订单';
+-- ----------------------------
+-- Table structure for t_news
+-- ----------------------------
+DROP TABLE IF EXISTS `t_news`;
+CREATE TABLE `t_news`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标题',
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '内容',
+  `is_deleted` tinyint(4) NULL DEFAULT 1 COMMENT '是否删除',
+  `gmt_create` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '新闻' ROW_FORMAT = Dynamic;
 
-CREATE TABLE `t_orderitem` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '商品id',
-  `quantity` int(11) DEFAULT NULL COMMENT '数量',
-  `order_id` bigint unsigned) DEFAULT NULL COMMENT '订单id',
-  `product` bigint unsigned DEFAULT NULL COMMENT '商品id',
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`product`) REFERENCES `t_product` (`id`),
-  FOREIGN KEY (`order_id`) REFERENCES `t_order` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '订单关联';
+-- ----------------------------
+-- Table structure for t_order
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order`;
+CREATE TABLE `t_order`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `order_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'id',
+  `status` int(11) NULL DEFAULT NULL COMMENT '状态',
+  `user_id` bigint(20) UNSIGNED NULL DEFAULT NULL COMMENT '购买人id',
+  `user_address` bigint(20) UNSIGNED NULL DEFAULT NULL COMMENT '收货地址',
+  `is_deleted` tinyint(4) NULL DEFAULT 1 COMMENT '是否删除',
+  `gmt_create` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `user_address`(`user_address`) USING BTREE,
+  INDEX `user_id`(`user_id`) USING BTREE,
+  CONSTRAINT `t_order_ibfk_1` FOREIGN KEY (`user_address`) REFERENCES `t_useraddress` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `t_order_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '订单' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_orderitem
+-- ----------------------------
+DROP TABLE IF EXISTS `t_orderitem`;
+CREATE TABLE `t_orderitem`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '商品id',
+  `quantity` int(11) NULL DEFAULT NULL COMMENT '数量',
+  `order_id` bigint(20) UNSIGNED NULL DEFAULT NULL COMMENT '订单id',
+  `product` bigint(20) UNSIGNED NULL DEFAULT NULL COMMENT '商品id',
+  `gmt_create` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `product`(`product`) USING BTREE,
+  INDEX `order_id`(`order_id`) USING BTREE,
+  CONSTRAINT `t_orderitem_ibfk_1` FOREIGN KEY (`product`) REFERENCES `t_product` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `t_orderitem_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `t_order` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '订单关联' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_product
+-- ----------------------------
+DROP TABLE IF EXISTS `t_product`;
+CREATE TABLE `t_product`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品标题',
+  `code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品码',
+  `model` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '型号',
+  `note` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品描述',
+  `pic_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品图片',
+  `price` int(11) NULL DEFAULT NULL COMMENT '价格',
+  `stock` bigint(20) NULL DEFAULT NULL COMMENT '库存',
+  `is_deleted` tinyint(4) NULL DEFAULT 1 COMMENT '是否删除',
+  `gmt_create` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '商品' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_producttype
+-- ----------------------------
+DROP TABLE IF EXISTS `t_producttype`;
+CREATE TABLE `t_producttype`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '类别名称',
+  `is_deleted` tinyint(4) NULL DEFAULT 1 COMMENT '是否删除',
+  `gmt_create` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '商品类别' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `account` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '余额',
+  `address` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '收货地址',
+  `password` varchar(14) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
+  `phone` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '电话号码',
+  `point` int(11) NULL DEFAULT NULL COMMENT '积分值',
+  `remark` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
+  `tel_phone` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '座机',
+  `username` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `zip_code` varchar(6) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮编',
+  `is_deleted` tinyint(4) NULL DEFAULT 1 COMMENT '是否删除',
+  `gmt_create` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_useraddress
+-- ----------------------------
+DROP TABLE IF EXISTS `t_useraddress`;
+CREATE TABLE `t_useraddress`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地址',
+  `consignee` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '收货人姓名',
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '收货电话',
+  `zipcode` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮编',
+  `user_id` bigint(20) UNSIGNED NULL DEFAULT NULL COMMENT '用户id',
+  `is_deleted` tinyint(4) NULL DEFAULT 1 COMMENT '是否删除',
+  `gmt_create` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `user_id`(`user_id`) USING BTREE,
+  CONSTRAINT `t_useraddress_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `t_user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '地址' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
